242c9a24938b5134649e0428482d82344cda180b
DrCowiber <38870762+DrCowiber@users.noreply.github.com>
2022-03-08 15:00:47 -0700

    CRIMEA

---
+++ JS/crimea.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+const quizStartButton = document.getElementById("quizStart");
+quizStartButton.onclick = function(){
+  console.log("YOUR MOM");
+};
+});
