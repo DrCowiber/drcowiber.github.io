82ca9172573ac0cd6b2edb1fa78c4934af86240e
DrCowiber <38870762+DrCowiber@users.noreply.github.com>
2022-03-11 14:00:42 -0700

    position quiz start

--- JS/crimea.js
+++ JS/crimea.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function() {
+const questionarreContent - document.getElementById("crimeaQuestionarre");
 const quizStartButton = document.getElementById("quizStart");
 quizStartButton.onclick = function(){
   console.log("YOUR MOM");
