390818fd6df520e24e4a61a357c614d37c4c78fd
DrCowiber <38870762+DrCowiber@users.noreply.github.com>
2022-03-18 14:58:15 -0600

    questoniarre
    The questionairre is functionally finished.
    The questionairre does not test answers yet. It will page through all questions and answers that are built into an array.

--- JS/crimea.js
+++ JS/crimea.js
@@ -1,36 +1,62 @@
 window.addEventListener("load", function() {
-const questionarreContent = document.getElementById("crimeaQuestionarre");
+const questionarreContent = document.getElementById("formSpace");
 const quizButton = document.getElementById("quizButton");
-const form = document.querySelector("form");
 
 answersObj = {
   answers1: ["42", "to die"],
   answers2: ["soviet russia", "eastern europe"]
 };
 const questions = ["What is the meaning of life?", "Where is ukraine located?"];
-
 let answerInput = [null];
 
 let questionCount = 0;
+let questionCreated = false;
 
 quizButton.onclick = function click(){
-  crimeaQuestionarre.innerHTML =
-  "<h1>" + questions[questionCount] + "</h1>" +
-  "<form>" +
-  "<input type='radio' name='question1' id='q1' value='" + answersObj['answers' + (questionCount + 1)][0] + "'>" +
-  "<label for='" + answersObj['answers' + (questionCount + 1)][0] + "'>" + answersObj['answers' + (questionCount + 1)][0] + "</label><br>" +
+  questionCreated = false;
+  try{//check if question exists then create or direct to catch statement
+    answersObj['answers' + (questionCount + 1)][0].prop
+    console.log("NEXT!");
+    formSpace.innerHTML =
+    "<h1>" + questions[questionCount] + "</h1>" +
+    "<form>" +
+    "</form>" ;
+    let answerCount = 0;
+    const form = document.querySelector("form");
+    while(questionCreated == false){
+    try{answersObj['answers' + (questionCount + 1)][answerCount].prop
 
-  "<input type='radio' name='question1' id='q2' value='" + answersObj['answers' + (questionCount + 1)][1] + "'>" +
-  "<label for='" + answersObj['answers' + (questionCount + 1)][1] + "'>" + answersObj['answers' + (questionCount + 1)][1] + "</label><br>" +
-  "<button type='button' id='quizButton' onclick='click()'> Submit </button>" +
-  "</form>" ;
+    //input creation and definition
+      input = document.createElement("input");
+      input.type = "radio";
+      input.name = "question";
+      input.id = "q1"
+      input.value = answersObj['answers' + (questionCount + 1)][questionCount];
+    //label creation and definition
+      label = document.createElement("label");
+      label.for = answersObj['answers' + (questionCount + 1)][answerCount];
+      label.innerHTML = answersObj['answers' + (questionCount + 1)][answerCount];
+    //appending of new elements
+      form.appendChild(input);
+      form.appendChild(label);
+      console.log("questionCount = " + questionCount);
+      console.log("answerCount = " + answerCount);
+      answerCount++;
+    }catch {//finished creating answer buttons
+      alert("Done!");
+      questionCreated = true;
+    }}
 
-
-    //
-    //
-
+    questionCount++;
+    console.log(questionCount);
+  } catch {//when there is no more questions, show ending message!
+    formSpace.innerHTML =
+    "<h1> Wow! you did so good! </h1>" +
+    "<img src='images/coatOfArms.png' width='25%'/> ";
+    quizButton.remove();
+  }
 };
-
+/*
 setInterval(function(){
   let formAnswer = new FormData(form);
   let output = "";
@@ -40,6 +66,5 @@ setInterval(function(){
   console.log(output);
   console.log(document.querySelector('input[name="rate"]:checked').value);
 
-}, 1000);
-
+}, 1000);*/
 });
